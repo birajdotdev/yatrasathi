7a6bb1267922efb3d62d60cebfb98c11c9d1cb83
Sthabiraj <birajshrestha2007@gmail.com>
2024-12-06 10:38:17 +0545

    ✨ feat: add framer-motion for animations, update Hero component with new design and background images, and configure VSCode settings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Footer from "@/components/footer";
 
 export default function LandingPage() {
   return (
-    <div className="flex min-h-screen flex-col bg-sky-50">
+    <div className="flex min-h-screen flex-col">
       <NavBar />
       <main className="flex-1">
         <Hero />
