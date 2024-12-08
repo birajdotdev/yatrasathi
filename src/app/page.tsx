f8d914635d532496716ec34e229fd27306f457ad
Sthabiraj <birajshrestha2007@gmail.com>
2024-12-08 21:09:43 +0545

    ✨ feat: remove unused imports from LandingPage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import NavBar from "@/components/nav/nav-bar";
 import {
   Hero,
   Benefits,
   Testimonials,
   Features,
 } from "@/components/landing-page";
-import Footer from "@/components/footer";
 
 export default function LandingPage() {
   return (
