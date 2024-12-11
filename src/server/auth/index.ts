49d79dd33b45e0a24ab389ca09032a6fb5828b09
Sthabiraj <birajshrestha2007@gmail.com>
2024-12-11 20:12:08 +0545

    ♻️ refactor: implement caching for authentication in NextAuth

--- src/server/auth/index.ts
+++ src/server/auth/index.ts
@@ -1,5 +1,10 @@
 import NextAuth from "next-auth";
+import { cache } from "react";
 
 import { authConfig } from "./config";
 
-export const { auth, handlers, signIn, signOut } = NextAuth(authConfig);
+const { auth: uncachedAuth, handlers, signIn, signOut } = NextAuth(authConfig);
+
+const auth = cache(uncachedAuth);
+
+export { auth, handlers, signIn, signOut };
