bbfc3947ae383da083d82f161b92f35078ad6f49
Sthabiraj <birajshrestha2007@gmail.com>
2024-12-30 09:13:21 +0545

    ✨ feat: implement session-based redirection in LandingPage component
    - Added authentication check in the LandingPage component to redirect users with an active session to the default login page.
    - Imported necessary modules for authentication and redirection functionality.
    - Enhanced user experience by preventing access to the landing page for logged-in users.

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,11 +1,19 @@
+import { redirect } from "next/navigation";
+
 import {
   Benefits,
   Features,
   Hero,
   Testimonials,
 } from "@/components/landing-page";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { auth } from "@/server/auth";
 
-export default function LandingPage() {
+export default async function LandingPage() {
+  const session = await auth();
+
+  if (session) return redirect(DEFAULT_LOGIN_REDIRECT);
+
   return (
     <>
       <Hero />
