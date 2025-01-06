130b8395b3e5b8e0a7b83ff67558f0a196e9007d
Sthabiraj <birajshrestha2007@gmail.com>
2025-01-06 19:53:07 +0545

    ✨ feat: add metadata to authentication and dashboard pages
    - Introduced metadata for the Signin, Dashboard, and Terms of Service pages to enhance SEO and user experience.
    - Updated SigninPage and DashboardPage components to include descriptive titles and descriptions.
    - Improved overall structure and clarity of the application by standardizing page metadata.

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,4 +1,11 @@
+import { type Metadata } from "next";
+
 import Signin from "@/components/auth/signin";
+
+export const metadata: Metadata = {
+  title: "Sign in",
+  description: "Sign in to your account",
+};
 
 export default function SigninPage() {
   return (
