6ad988944f1c314c2aa907bca0c4dd9e4f75aa62
birajdotdev <birajdotdev@gmail.com>
2025-06-03 15:17:20 +0545

    🔧 refactor: streamline user management and API interactions
    - Removed unused syncClerkUserMetadata function and related imports to clean up the codebase.
    - Updated user creation and update procedures to utilize new schema methods for improved validation and error handling.
    - Enhanced user deletion logic with better error handling and transaction management.
    - Added unique index for clerkUserId in the user schema to ensure data integrity.

--- src/server/auth/index.ts
+++ src/server/auth/index.ts
@@ -1,35 +1,5 @@
 import { cache } from "react";
 
-import { clerkClient, auth as uncachedAuth } from "@clerk/nextjs/server";
-
-import { type UserRole } from "@/server/db/schema";
-
-const client = await clerkClient();
+import { auth as uncachedAuth } from "@clerk/nextjs/server";
 
 export const auth = cache(uncachedAuth);
-
-export const getCurrentUser = cache(async () => {
-  const { userId, sessionClaims, redirectToSignIn, has } = await auth();
-  const isProUser = has({ plan: "pro" });
-
-  return {
-    clerkUserId: userId,
-    dbId: sessionClaims?.dbId,
-    role: sessionClaims?.role,
-    isProUser,
-    redirectToSignIn,
-  };
-});
-
-export function syncClerkUserMetadata(user: {
-  id: string;
-  clerkUserId: string;
-  role: UserRole;
-}) {
-  return client.users.updateUserMetadata(user.clerkUserId, {
-    publicMetadata: {
-      dbId: user.id,
-      role: user.role,
-    },
-  });
-}
