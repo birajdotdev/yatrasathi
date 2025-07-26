ef8b30d78756253acb4b6588486f6f927beb6498
birajdotdev <birajdotdev@gmail.com>
2025-07-26 13:41:50 +0545

    ✨ feat: enhance authentication handling and user context in TRPC procedures

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -5,31 +5,23 @@ import AppSidebar from "@/components/sidebar";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { auth } from "@/server/auth";
-import { HydrateClient, api } from "@/trpc/server";
 
 export default async function ProtectedLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  await Promise.all([
-    void api.user.getReminderPreferences.prefetch(),
-    void api.notification.getNotifications.prefetch(),
-  ]);
   const { userId, redirectToSignIn } = await auth();
   if (!userId) return redirectToSignIn();
-
   return (
-    <HydrateClient>
-      <SidebarProvider>
-        <AppSidebar />
-        <SidebarInset>
-          <DashboardNav />
-          <main className="flex-1 overflow-hidden">
-            <ScrollArea className="h-[calc(100vh-5rem)]">{children}</ScrollArea>
-          </main>
-        </SidebarInset>
-      </SidebarProvider>
-    </HydrateClient>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <DashboardNav />
+        <main className="flex-1 overflow-hidden">
+          <ScrollArea className="h-[calc(100vh-5rem)]">{children}</ScrollArea>
+        </main>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
